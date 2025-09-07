65f2ccde42567bbf8dc33b82e2f8e827a0d3a6ce
reii <elaisaa84@gmail.com>
2025-09-07 20:11:54 +0800

    Add files via upload

---
+++ sybau database/sybau database/Dashboard.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace sybau_database
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBoxName = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtBoxEmail = new System.Windows.Forms.TextBox();
+            this.tableData = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.tableData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtBoxName
+            // 
+            this.txtBoxName.Location = new System.Drawing.Point(281, 54);
+            this.txtBoxName.Name = "txtBoxName";
+            this.txtBoxName.Size = new System.Drawing.Size(154, 20);
+            this.txtBoxName.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.Transparent;
+            this.btnAdd.BackgroundImage = global::sybau_database.Properties.Resources.rigby_bg;
+            this.btnAdd.Location = new System.Drawing.Point(360, 128);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.BackColor = System.Drawing.Color.Transparent;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.lblName.Location = new System.Drawing.Point(212, 55);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(41, 15);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "Name";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.BackColor = System.Drawing.Color.Transparent;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.lblEmail.Location = new System.Drawing.Point(212, 92);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(39, 15);
+            this.lblEmail.TabIndex = 2;
+            this.lblEmail.Text = "Email";
+            // 
+            // txtBoxEmail
+            // 
+            this.txtBoxEmail.Location = new System.Drawing.Point(281, 91);
+            this.txtBoxEmail.Name = "txtBoxEmail";
+            this.txtBoxEmail.Size = new System.Drawing.Size(154, 20);
+            this.txtBoxEmail.TabIndex = 0;
+            // 
+            // tableData
+            // 
+            this.tableData.AllowUserToAddRows = false;
+            this.tableData.AllowUserToDeleteRows = false;
+            this.tableData.AllowUserToResizeColumns = false;
+            this.tableData.AllowUserToResizeRows = false;
+            this.tableData.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.tableData.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tableData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tableData.Location = new System.Drawing.Point(111, 196);
+            this.tableData.Name = "tableData";
+            this.tableData.ReadOnly = true;
+            this.tableData.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.tableData.Size = new System.Drawing.Size(441, 176);
+            this.tableData.TabIndex = 3;
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::sybau_database.Properties.Resources.rigby_bg;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(664, 442);
+            this.Controls.Add(this.tableData);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtBoxEmail);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtBoxName);
+            this.DoubleBuffered = true;
+            this.Name = "Dashboard";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dashboard";
+            this.Load += new System.EventHandler(this.Dashboard_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tableData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBoxName;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtBoxEmail;
+        private System.Windows.Forms.DataGridView tableData;
+    }
+}
+
---
+++ sybau database/sybau database/Dashboard.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace sybau_database
+{
+    public partial class Dashboard : Form
+    {
+        private string connectionString = "Data Source=REIREII;Initial Catalog=SybauDB;Integrated Security=True;TrustServerCertificate=True;";
+
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("INSERT INTO Users (Name, Email) VALUES (@Name, @Email)", conn);
+                cmd.Parameters.AddWithValue("@Name", txtBoxName.Text);
+                cmd.Parameters.AddWithValue("@Email", txtBoxEmail.Text);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("User added successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                LoadData();
+            }
+        }
+
+        private void Dashboard_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Users", conn);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                tableData.DataSource = dt;
+            }
+        }
+    }
+}
